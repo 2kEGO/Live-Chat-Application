6175bddb6402bcb5fdfe3f6d5cc6740d537965e1
Long Nguyen <barnonguyen@gmail.com>
2025-04-24 21:06:09 -0400

    update

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -3,6 +3,8 @@ import {auth, provider} from '../firebase-config'
 import { signInWithPopup} from 'firebase/auth'
 import Cookies from 'universal-cookie'
 const cookies = new Cookies()
+import { addDoc, setDoc, doc, collection } from 'firebase/firestore'
+import { db } from '../firebase-config'
 
 
 const Auth = () => {
@@ -13,6 +15,18 @@ const Auth = () => {
             const result = await signInWithPopup(auth, provider)
             console.log(result)
             cookies.set("auth-token",result.user.refreshToken)
+
+
+            await setDoc(doc(db, "users", result.user.uid), {
+                displayName: result.user.displayName,
+                email: result.user.email,
+                photoUrl: result.user.photoURL,
+                uid: result.user.uid
+              }, { merge: true })
+
+            await setDoc(doc(db, "userChats", result.user.uid), {});
+            
+
         } catch (error) {
             console.error(error)
         }
